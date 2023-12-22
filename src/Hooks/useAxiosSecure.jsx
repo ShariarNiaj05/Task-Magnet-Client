5706d11c303583a301888425c28edd42efba2691
Shariar Islam <shariarn85@gmail.com>
2023-12-22 18:49:13 +0600

    production link added

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -5,8 +5,8 @@ import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-  // baseURL: "https://task-magnet-server.vercel.app", 
-  baseURL: "http://localhost:5000",
+  baseURL: "https://task-magnet-server.vercel.app", 
+  // baseURL: "http://localhost:5000",
 //   withCredentials: true,
 });
 
